da7190f9b1d1453a5772b92ec365fe240415aa53
Adam E <adam.elyas99@gmail.com>
2021-11-08 01:08:21 +0800

    Run cache_attendance immediately and another at 8am MST daily
    Signed-off-by: Adam E <adam.elyas99@gmail.com>

--- mmls_attendance_grabber/app/apps.py
+++ mmls_attendance_grabber/app/apps.py
@@ -12,15 +12,22 @@ class AppConfig(AppConfig):
         from background_task.models import Task
         from . import views, tasks
         try:
-            if not Task.objects.filter(verbose_name="cache_attendance").exists():
-                # Schedule a day later from now at 8am
-                tz = datetime.timezone(datetime.timedelta(hours=8))
-                datetime_later = datetime.datetime.now(tz).replace(hour=8, minute=0, second=0, microsecond=0)
+            if Task.objects.filter(verbose_name="cache_attendance").exists():
+                for cache_task in Task.objects.filter(verbose_name="cache_attendance"):
+                    cache_task.delete()
 
-                while datetime.datetime.now(tz) > datetime_later:
-                    datetime_later += datetime.timedelta(days=1)
+            # Schedule a day later from now at 8am
+            tz = datetime.timezone(datetime.timedelta(hours=8))
+            datetime_later = datetime.datetime.now(tz).replace(hour=8, minute=0, second=0, microsecond=0)
 
-                tasks.update_cache(schedule=datetime_later, repeat=Task.DAILY, verbose_name="cache_attendance")
+            while datetime.datetime.now(tz) > datetime_later:
+                datetime_later += datetime.timedelta(days=1)
+
+            # Run now
+            tasks.update_cache(schedule=0, verbose_name="cache_attendance")
+
+            # Run at 8AM MST
+            tasks.update_cache(schedule=datetime_later, repeat=Task.DAILY, verbose_name="cache_attendance")
         except ProgrammingError as e:
             # Error during migration, ignore.
             logging.info(f"Ignoring exception: {repr(e)}")
